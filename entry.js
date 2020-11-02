4113fd118a025a3b15cad3f27850e2a899f93753
Sai Vishwak Gangam <70585835+saivishwakgangam@users.noreply.github.com>
2020-11-02 13:12:35 +0530

    Add files via upload

---
+++ entry.js
@@ -0,0 +1,28 @@
+$('body').append('<div style="z-index: 999;" id="loadingDiv"><img class="displayed" src="https://www.iiit.ac.in/img/iiit-new.png"/><div class="loader"></div></div></center>');
+$(window).on('load', function(){
+    setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+  });
+  function removeLoader(){
+      $( "#loadingDiv" ).fadeOut(500, function() {
+        // fadeOut complete. Remove the loading div
+        $( "#loadingDiv" ).remove(); //makes page more lightweight 
+    });  
+  }
+let requestURL = 'https://rawcdn.githack.com/saivishwakgangam/SSD-LAB-ACTIVITY-2/f3da24ed67ea4bc780860509b1e2d2fb26c83027/research_centers%20.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'text';
+    request.send();
+    request.onload = function() {
+    const researchcenterstext = request.response; // get the string from the response
+    const researchcenters = JSON.parse(researchcenterstext);
+    
+
+   //document.write(researchcenters['SERC']);
+    for (var p in researchcenters) {
+        if( researchcenters.hasOwnProperty(p) ) {
+            $(".container").append(`<div class='box'><div class='content'><center><h3>${p}</h3></center></div></div>`);
+        } 
+     }
+    }
+    var x = document.getElementById("").textContent; 
