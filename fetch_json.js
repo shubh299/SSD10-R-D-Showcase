c150a9f9b6e0d6aab5f512110a30adb77f76a03e
Sanyam Jain <sanyam.jain@students.iiit.ac.in>
2020-11-17 16:34:27 +0530

    Javascript file added to fetch json data

---
+++ fetch_json.js
@@ -0,0 +1,52 @@
+window.static_url = "https://rapid-reminiscent-servant.glitch.me/research_centers.json";
+
+function fetch_json_object(){
+    var result = "";
+    $.ajax({
+        url: static_url,
+        type:"get",
+        dataType:'json',
+        async: false,
+        success: function(centers){
+          result = centers;
+        },
+        error:function() {
+          console.log("err");
+        }
+    });
+    return result;
+}
+
+function fetch_poster_by_room_name(room_name){
+    var result = "";
+    $.ajax({
+        url: static_url,
+        type:"get",
+        dataType:'json',
+        async: false,
+        success: function(centers){
+          result = centers[room_name];
+        },
+        error:function() {
+          console.log("err");
+        }
+    });
+    return result;
+}
+
+function fetch_room_names(){
+    var result = "";
+    $.ajax({
+        url: static_url,
+        type:"get",
+        dataType:'json',
+        async: false,
+        success: function(centers){
+          result = Object.keys(centers);
+        },
+        error:function() {
+          console.log("err");
+        }
+    });
+    return result;  
+}
